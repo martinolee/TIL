328787f59294c6e046e35a5a59d131ddb22bded6
martinolee <martino_lee@naver.com>
2019-12-17 22:51:49 +0900

    add task for 2019.12.17

---
+++ swift/2019.12/17/UITableViewExample/UITableViewExample/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  UITableViewExample
+//
+//  Created by Soohan Lee on 2019/12/17.
+//  Copyright © 2019 Soohan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = "\(indexPath.row)"
+        
+        return cell
+    }
+    
+    
+}
