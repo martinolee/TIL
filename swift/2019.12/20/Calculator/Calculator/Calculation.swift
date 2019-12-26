03bfec45968ccba8333e8d2eeec25cfd881ec75b
martinolee <martino_lee@naver.com>
2019-12-26 21:31:26 +0900

    add task

---
+++ swift/2019.12/20/Calculator/Calculator/Calculation.swift
@@ -0,0 +1,13 @@
+//
+//  Calculation.swift
+//  Calculator
+//
+//  Created by Soohan Lee on 2019/12/20.
+//  Copyright © 2019 Soohan. All rights reserved.
+//
+
+import Foundation
+
+class Calculation {
+    
+}
---
+++ swift/2019.12/20/Calculator/Calculator/CalculatorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalculatorViewController.swift
+//  Calculator
+//
+//  Created by Soohan Lee on 2019/12/20.
+//  Copyright © 2019 Soohan. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
