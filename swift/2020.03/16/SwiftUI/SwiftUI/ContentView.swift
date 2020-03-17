6e10a4e54832f34b3bc5c6eff9c609e6f5604acb
martinolee <martino_lee@naver.com>
2020-03-17 14:05:39 +0900

    update

---
+++ swift/2020.03/16/SwiftUI/SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI
+//
+//  Created by Soohan Lee on 2020/03/16.
+//  Copyright © 2020 Soohan Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
