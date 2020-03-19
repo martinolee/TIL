1d81df34e737f90413abafeaa220142655146dff
martinolee <martino_lee@naver.com>
2020-03-19 18:31:21 +0900

    pratice libraries

--- swift/2020.03/19/LibrariesPractice/LibrariesPractice/ViewController.swift
+++ swift/2020.03/19/LibrariesPractice/LibrariesPractice/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import Alamofire
+import Kingfisher
+import SnapKit
+import Then
 
 class ViewController: UIViewController {
-
+  private let swiftImageView = UIImageView().then {
+    $0.contentMode = .scaleAspectFit
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    view.addSubview(swiftImageView)
+    setupSwiftImageViewAutoLayout()
+    
+    let imageURL = URL(string: "https://developer.apple.com/swift/images/swift-og.png")!
+    let image = ImageResource(downloadURL: imageURL)
+    swiftImageView.kf.setImage(with: image)
+    
+    requestData("https://developer.apple.com/swift/images/swift-og.png") { result in
+      switch result {
+      case .success(let data):
+        print(data)
+      case .failure(let error):
+        print(error)
+      }
+    }
   }
-
-
+  
+  private func setupSwiftImageViewAutoLayout() {
+    swiftImageView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+  }
+  
+  private func requestData(_ url: String, completionHandler: @escaping (Result<Data, Error>) -> Void) {
+    AF.request(url)
+      .validate()
+      .responseData { response in
+        switch response.result {
+        case .success(let data):
+          completionHandler(.success(data))
+        case .failure(let error):
+          completionHandler(.failure(error))
+        }
+    }
+  }
 }
-
---
+++ swift/2020.03/19/LibrariesPractice/Pods/Target Support Files/Pods-LibrariesPractice/Pods-LibrariesPractice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LibrariesPractice : NSObject
+@end
+@implementation PodsDummy_Pods_LibrariesPractice
+@end
